2f2734231aea1dc583a0270a05182639f4903343
nui <monihungrykids@gmmail.com>
2025-10-14 23:25:55 +1300

    still playing around wth the flame

--- 1Draw_hands.js
+++ 1Draw_hands.js
@@ -7,46 +7,57 @@ function prepareInteraction() {
 function drawInteraction(faces, hands) {
   // hands part
   // for loop to capture if there is more than one hand on the screen. This applies the same process to all hands.
-push();
-translate (200, 100);
-flame ();
-pop();
   for (let i = 0; i < hands.length; i++) {
     let hand = hands[i];
+    //console.log(hand);
     if (showKeypoints) {
       drawConnections(hand)
     }
 
-    // Gesture detection (assume detectHandGesture is available)
-    let gesture = typeof detectHandGesture === 'function' ? detectHandGesture(hand) : null;
+    // This is how to load in the x and y of a point on the hand.
+    let indexFingerTipX = hand.index_finger_tip.x;
+    let indexFingerTipY = hand.index_finger_tip.y;
+    let wristX = hand.wrist.x;
+    let wristY = hand.wrist.y;
+    let wristZ = hand.wrist.z3D;
+    let middleFingerTipX = hand.middle_finger_tip.x;
+let middleFingerTipY = hand.middle_finger_tip.y;
+let middleFingerTipZ = hand.middle_finger_tip.z3D;
 
-    // Palm position
-    let palmX = hand.palm ? hand.palm.x : hand.wrist.x;
-    let palmY = hand.palm ? hand.palm.y : hand.wrist.y;
+let thumbTipX = hand.thumb_tip.x;
+let thumbTipY = hand.thumb_tip.y;
+let thumbTipZ = hand.thumb_tip.z3D;
 
-    if (gesture === 'Fist') {
-      // Draw a big flame at the palm
-      let angle = 0; // Upright
-      push();
-      scale(2.2); // Make the flame bigger (adjust as needed)
-      flame(palmX / 2.2, palmY / 2.2, angle);
-      pop();
-    } else {
-      // Draw individual finger flames
-      let indexFingerTipX = hand.index_finger_tip.x;
-      let indexFingerTipY = hand.index_finger_tip.y;
-      let middleFingerTipX = hand.middle_finger_tip.x;
-      let middleFingerTipY = hand.middle_finger_tip.y;
-      let thumbTipX = hand.thumb_tip.x;
-      let thumbTipY = hand.thumb_tip.y;
-      let middleFingerPipX = hand.middle_finger_pip.x;
-      let middleFingerPipY = hand.middle_finger_pip.y;
-      let angle = Math.atan2(middleFingerTipY - middleFingerPipY, middleFingerTipX - middleFingerPipX);
-      flame(middleFingerTipX, middleFingerTipY, angle);
-      flame(indexFingerTipX, indexFingerTipY, angle);
-      flame(thumbTipX, thumbTipY, angle);
-    }
-    // ...existing code...
+    //  let pinkyFingerTipX = hand.pinky_finger_tip.x;
+    //  let pinkyFingerTipY = hand.pinky  _finger_tip.y;
+
+    /*
+    Start drawing on the hands here
+    */
+  
+  
+    //this is ai/co pilot.
+  // Calculate angle between middle finger tip and pip (proximal interphalangeal joint)
+  let middleFingerPipX = hand.middle_finger_pip.x;
+  let middleFingerPipY = hand.middle_finger_pip.y;
+  let angle = Math.atan2(middleFingerTipY - middleFingerPipY, middleFingerTipX - middleFingerPipX);
+  flame(middleFingerTipX, middleFingerTipY, angle);
+
+
+    //drawPoints(hand)
+
+  flame (indexFingerTipX, indexFingerTipY, angle);
+flame (thumbTipX, thumbTipY, angle)
+
+push();
+translate (200, 100);
+flame ();
+pop();
+    //chameleonHandPuppet(hand)
+
+    /*
+    Stop drawing on the hands here
+    */
   }
   // You can make addtional elements here, but keep the hand drawing inside the for loop. 
   //------------------------------------------------------
