086b4b880e5f1b5935ee3efb3789ba4fc492ddd6
nui <monihungrykids@gmmail.com>
2025-10-17 17:08:50 +1300

    my attempt, wow.

--- 1Draw_hands.js
+++ 1Draw_hands.js
@@ -15,35 +15,107 @@ function drawInteraction(faces, hands) {
     }
 
     // This is how to load in the x and y of a point on the hand.
-    let indexFingerTipX = hand.index_finger_tip.x;
-    let indexFingerTipY = hand.index_finger_tip.y;
+   let handedness = hand.handedness; // "Left" or "Right"
+// Wrist
+let wristX = hand.wrist.x;
+let wristY = hand.wrist.y;
+let wristZ = hand.wrist.z3D;
 
-    let wristX = hand.wrist.x;
-    let wristY = hand.wrist.y;
-    let wristZ = hand.wrist.z3D;
+// Thumb
+let thumbCmcX = hand.thumb_cmc.x;
+let thumbCmcY = hand.thumb_cmc.y;
+let thumbCmcZ = hand.thumb_cmc.z3D;
 
+let thumbMcpX = hand.thumb_mcp.x;
+let thumbMcpY = hand.thumb_mcp.y;
+let thumbMcpZ = hand.thumb_mcp.z3D;
 
-    let middleFingerTipX = hand.middle_finger_tip.x;
-    let middleFingerTipY = hand.middle_finger_tip.y;
-    let middleFingerTipZ = hand.middle_finger_tip.z3D;
+let thumbIpX = hand.thumb_ip.x;
+let thumbIpY = hand.thumb_ip.y;
+let thumbIpZ = hand.thumb_ip.z3D;
 
 let thumbTipX = hand.thumb_tip.x;
 let thumbTipY = hand.thumb_tip.y;
 let thumbTipZ = hand.thumb_tip.z3D;
 
+// Index finger
+let indexFingerMcpX = hand.index_finger_mcp.x;
+let indexFingerMcpY = hand.index_finger_mcp.y;
+let indexFingerMcpZ = hand.index_finger_mcp.z3D;
+
+let indexFingerPipX = hand.index_finger_pip.x;
+let indexFingerPipY = hand.index_finger_pip.y;
+let indexFingerPipZ = hand.index_finger_pip.z3D;
+
+let indexFingerDipX = hand.index_finger_dip.x;
+let indexFingerDipY = hand.index_finger_dip.y;
+let indexFingerDipZ = hand.index_finger_dip.z3D;
+
+let indexFingerTipX = hand.index_finger_tip.x;
+let indexFingerTipY = hand.index_finger_tip.y;
+let indexFingerTipZ = hand.index_finger_tip.z3D;
+
+// Middle finger
+let middleFingerMcpX = hand.middle_finger_mcp.x;
+let middleFingerMcpY = hand.middle_finger_mcp.y;
+let middleFingerMcpZ = hand.middle_finger_mcp.z3D;
+
+let middleFingerPipX = hand.middle_finger_pip.x;
+let middleFingerPipY = hand.middle_finger_pip.y;
+let middleFingerPipZ = hand.middle_finger_pip.z3D;
+
+let middleFingerDipX = hand.middle_finger_dip.x;
+let middleFingerDipY = hand.middle_finger_dip.y;
+let middleFingerDipZ = hand.middle_finger_dip.z3D;
+
+let middleFingerTipX = hand.middle_finger_tip.x;
+let middleFingerTipY = hand.middle_finger_tip.y;
+let middleFingerTipZ = hand.middle_finger_tip.z3D;
+
+// Ring finger
+let ringFingerMcpX = hand.ring_finger_mcp.x;
+let ringFingerMcpY = hand.ring_finger_mcp.y;
+let ringFingerMcpZ = hand.ring_finger_mcp.z3D;
+
+let ringFingerPipX = hand.ring_finger_pip.x;
+let ringFingerPipY = hand.ring_finger_pip.y;
+let ringFingerPipZ = hand.ring_finger_pip.z3D;
+
+let ringFingerDipX = hand.ring_finger_dip.x;
+let ringFingerDipY = hand.ring_finger_dip.y;
+let ringFingerDipZ = hand.ring_finger_dip.z3D;
+
 let ringFingerTipX = hand.ring_finger_tip.x;
 let ringFingerTipY = hand.ring_finger_tip.y;
 let ringFingerTipZ = hand.ring_finger_tip.z3D;
+
+// Pinky finger
+let pinkyFingerMcpX = hand.pinky_finger_mcp.x;
+let pinkyFingerMcpY = hand.pinky_finger_mcp.y;
+let pinkyFingerMcpZ = hand.pinky_finger_mcp.z3D;
+
+let pinkyFingerPipX = hand.pinky_finger_pip.x;
+let pinkyFingerPipY = hand.pinky_finger_pip.y;
+let pinkyFingerPipZ = hand.pinky_finger_pip.z3D;
+
+let pinkyFingerDipX = hand.pinky_finger_dip.x;
+let pinkyFingerDipY = hand.pinky_finger_dip.y;
+let pinkyFingerDipZ = hand.pinky_finger_dip.z3D;
 
 let pinkyFingerTipX = hand.pinky_finger_tip.x;
 let pinkyFingerTipY = hand.pinky_finger_tip.y;
 let pinkyFingerTipZ = hand.pinky_finger_tip.z3D;
 
+let palmBaseX = (hand.index_finger_mcp.x + hand.pinky_finger_mcp.x + hand.wrist.x) / 3;
+let palmBaseY = (hand.index_finger_mcp.y + hand.pinky_finger_mcp.y + hand.wrist.y) / 3;
+let handOpen = dist(indexFingerTipX, indexFingerTipY, thumbTipX, thumbTipY);
+let openNorm = constrain(map(handOpen, 20, 200, 0, 1), 0, 1);
+let flameX = lerp(palmBaseX, indexFingerTipX, openNorm);
+let flameY = lerp(palmBaseY, indexFingerTipY, openNorm);
 
   //Start drawing on the hands here
   
-
-  
+flame(flameX, flameY, random(TWO_PI))
 
     /*
     Stop drawing on the hands here
@@ -64,26 +136,22 @@ function flame (x, y, angle){
   noStroke ();
 
   //outer glow part
-  fill (255, 80, 0);//orangey red
+  fill (255, 80, 0, 100)//(255, 80, 0);//orangey red
   ellipse (0, 0, 80, 100);
   //main bit
-  fill (255, 140, 0);//warm orange?
+  fill (255, 140, 0, 180);//(255, 140, 0);//warm orange?
   beginShape ();
   vertex (0, 40);
   bezierVertex (-20, 10, -10, -40, 0, -60);
   bezierVertex (10, -40, 20, 10, 0, 40);
   endShape (CLOSE);
   //inner bit
-  fill (255, 255, 0);//yellow
+  fill (255, 255, 0, 220)//(255, 255, 0);//yellow
   beginShape();
   vertex (0, 30);
   bezierVertex (-10, 5, -5, -30, 0, -40);
   bezierVertex (5, -30, 10, 5, 0, 30);
   endShape (CLOSE);
-  //core bit
-  fill (255, 0, 0)//soft red
-  ellipse (0, 20, 30, 40);
-
   pop();
   //
 
