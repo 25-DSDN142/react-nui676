95bc97f120d8d1e000be7ac195305e1e216ef8e6
nui <monihungrykids@gmmail.com>
2025-10-17 16:47:35 +1300

    restarting

--- 1Draw_hands.js
+++ 1Draw_hands.js
@@ -43,7 +43,7 @@ let pinkyFingerTipZ = hand.pinky_finger_tip.z3D;
   //Start drawing on the hands here
   
 
-    //chameleonHandPuppet(hand)
+  
 
     /*
     Stop drawing on the hands here
@@ -52,4 +52,39 @@ let pinkyFingerTipZ = hand.pinky_finger_tip.z3D;
   // You can make addtional elements here, but keep the hand drawing inside the for loop. 
 
   //------------------------------------------------------
+}
+
+function flame (x, y, angle){
+  push ();
+  translate (x, y);
+  rotate (angle);
+
+  let flicker = random(0.9, 1.1);
+  scale (flicker);
+  noStroke ();
+
+  //outer glow part
+  fill (255, 80, 0);//orangey red
+  ellipse (0, 0, 80, 100);
+  //main bit
+  fill (255, 140, 0);//warm orange?
+  beginShape ();
+  vertex (0, 40);
+  bezierVertex (-20, 10, -10, -40, 0, -60);
+  bezierVertex (10, -40, 20, 10, 0, 40);
+  endShape (CLOSE);
+  //inner bit
+  fill (255, 255, 0);//yellow
+  beginShape();
+  vertex (0, 30);
+  bezierVertex (-10, 5, -5, -30, 0, -40);
+  bezierVertex (5, -30, 10, 5, 0, 30);
+  endShape (CLOSE);
+  //core bit
+  fill (255, 0, 0)//soft red
+  ellipse (0, 20, 30, 40);
+
+  pop();
+  //
+
 }
