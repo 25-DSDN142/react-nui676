0c022e7b7b3e654194a286a897794acda8168d86
nui <monihungrykids@gmmail.com>
2025-10-20 10:36:38 +1300

    maxed out co pilot

--- 1Draw_hands.js
+++ 1Draw_hands.js
@@ -7,152 +7,112 @@ function prepareInteraction() {
 function drawInteraction(faces, hands) {
   // hands part
   // for loop to capture if there is more than one hand on the screen. This applies the same process to all hands.
+  if (hands.length === 2) {
+    // Get middle finger tips
+    let m1 = hands[0].middle_finger_tip;
+    let m2 = hands[1].middle_finger_tip;
+    let distMF = dist(m1.x, m1.y, m2.x, m2.y);
+    // Get palm centers
+    let palm1 = (hands[0].index_finger_mcp.x + hands[0].middle_finger_mcp.x + hands[0].ring_finger_mcp.x + hands[0].pinky_finger_mcp.x + hands[0].wrist.x) / 5;
+    let palm2 = (hands[1].index_finger_mcp.x + hands[1].middle_finger_mcp.x + hands[1].ring_finger_mcp.x + hands[1].pinky_finger_mcp.x + hands[1].wrist.x) / 5;
+    let palm1Y = (hands[0].index_finger_mcp.y + hands[0].middle_finger_mcp.y + hands[0].ring_finger_mcp.y + hands[0].pinky_finger_mcp.y + hands[0].wrist.y) / 5;
+    let palm2Y = (hands[1].index_finger_mcp.y + hands[1].middle_finger_mcp.y + hands[1].ring_finger_mcp.y + hands[1].pinky_finger_mcp.y + hands[1].wrist.y) / 5;
+    // Midpoint between palms
+    let midX = (palm1 + palm2) / 2;
+    let midY = (palm1Y + palm2Y) / 2;
+    // Map distance to scale
+    let minDist = 50, maxDist = 300;
+    let minScale = 0.7, maxScale = 2.5;
+    let scaleFactor = map(distMF, minDist, maxDist, minScale, maxScale, true);
+    // If hands are close, draw one flame
+    if (distMF < 150) {
+      flame(midX, midY, random(TWO_PI), scaleFactor, true, false);
+    } else {
+      // Otherwise, draw individual blue flames on each palm
+      flame(palm1, palm1Y, random(TWO_PI), 1, true, false);
+      flame(palm2, palm2Y, random(TWO_PI), 1, true, false);
+    }
+    return;
+  }
+  // Single hand or more than two: normal gesture logic
   for (let i = 0; i < hands.length; i++) {
     let hand = hands[i];
-    //console.log(hand);
-    if (showKeypoints) {
-      drawConnections(hand)
+    if (showKeypoints) drawConnections(hand);
+    let gesture = detectHandGesture(hand);
+    if (gesture === "Peace") {
+      flame(hand.index_finger_tip.x, hand.index_finger_tip.y, random(TWO_PI), 1, false, true);
+      flame(hand.middle_finger_tip.x, hand.middle_finger_tip.y, random(TWO_PI), 1, false, true);
+      continue;
     }
-
-    // This is how to load in the x and y of a point on the hand.
-   let handedness = hand.handedness; // "Left" or "Right"
-// Wrist
-let wristX = hand.wrist.x;
-let wristY = hand.wrist.y;
-let wristZ = hand.wrist.z3D;
-
-// Thumb
-let thumbCmcX = hand.thumb_cmc.x;
-let thumbCmcY = hand.thumb_cmc.y;
-let thumbCmcZ = hand.thumb_cmc.z3D;
-
-let thumbMcpX = hand.thumb_mcp.x;
-let thumbMcpY = hand.thumb_mcp.y;
-let thumbMcpZ = hand.thumb_mcp.z3D;
-
-let thumbIpX = hand.thumb_ip.x;
-let thumbIpY = hand.thumb_ip.y;
-let thumbIpZ = hand.thumb_ip.z3D;
-
-let thumbTipX = hand.thumb_tip.x;
-let thumbTipY = hand.thumb_tip.y;
-let thumbTipZ = hand.thumb_tip.z3D;
-
-// Index finger
-let indexFingerMcpX = hand.index_finger_mcp.x;
-let indexFingerMcpY = hand.index_finger_mcp.y;
-let indexFingerMcpZ = hand.index_finger_mcp.z3D;
-
-let indexFingerPipX = hand.index_finger_pip.x;
-let indexFingerPipY = hand.index_finger_pip.y;
-let indexFingerPipZ = hand.index_finger_pip.z3D;
-
-let indexFingerDipX = hand.index_finger_dip.x;
-let indexFingerDipY = hand.index_finger_dip.y;
-let indexFingerDipZ = hand.index_finger_dip.z3D;
-
-let indexFingerTipX = hand.index_finger_tip.x;
-let indexFingerTipY = hand.index_finger_tip.y;
-let indexFingerTipZ = hand.index_finger_tip.z3D;
-
-// Middle finger
-let middleFingerMcpX = hand.middle_finger_mcp.x;
-let middleFingerMcpY = hand.middle_finger_mcp.y;
-let middleFingerMcpZ = hand.middle_finger_mcp.z3D;
-
-let middleFingerPipX = hand.middle_finger_pip.x;
-let middleFingerPipY = hand.middle_finger_pip.y;
-let middleFingerPipZ = hand.middle_finger_pip.z3D;
-
-let middleFingerDipX = hand.middle_finger_dip.x;
-let middleFingerDipY = hand.middle_finger_dip.y;
-let middleFingerDipZ = hand.middle_finger_dip.z3D;
-
-let middleFingerTipX = hand.middle_finger_tip.x;
-let middleFingerTipY = hand.middle_finger_tip.y;
-let middleFingerTipZ = hand.middle_finger_tip.z3D;
-
-// Ring finger
-let ringFingerMcpX = hand.ring_finger_mcp.x;
-let ringFingerMcpY = hand.ring_finger_mcp.y;
-let ringFingerMcpZ = hand.ring_finger_mcp.z3D;
-
-let ringFingerPipX = hand.ring_finger_pip.x;
-let ringFingerPipY = hand.ring_finger_pip.y;
-let ringFingerPipZ = hand.ring_finger_pip.z3D;
-
-let ringFingerDipX = hand.ring_finger_dip.x;
-let ringFingerDipY = hand.ring_finger_dip.y;
-let ringFingerDipZ = hand.ring_finger_dip.z3D;
-
-let ringFingerTipX = hand.ring_finger_tip.x;
-let ringFingerTipY = hand.ring_finger_tip.y;
-let ringFingerTipZ = hand.ring_finger_tip.z3D;
-
-// Pinky finger
-let pinkyFingerMcpX = hand.pinky_finger_mcp.x;
-let pinkyFingerMcpY = hand.pinky_finger_mcp.y;
-let pinkyFingerMcpZ = hand.pinky_finger_mcp.z3D;
-
-let pinkyFingerPipX = hand.pinky_finger_pip.x;
-let pinkyFingerPipY = hand.pinky_finger_pip.y;
-let pinkyFingerPipZ = hand.pinky_finger_pip.z3D;
-
-let pinkyFingerDipX = hand.pinky_finger_dip.x;
-let pinkyFingerDipY = hand.pinky_finger_dip.y;
-let pinkyFingerDipZ = hand.pinky_finger_dip.z3D;
-
-let pinkyFingerTipX = hand.pinky_finger_tip.x;
-let pinkyFingerTipY = hand.pinky_finger_tip.y;
-let pinkyFingerTipZ = hand.pinky_finger_tip.z3D;
-
-
-// Place flame in the middle of the palm (average of MCP joints and wrist)
-let palmCenterX = (hand.index_finger_mcp.x + hand.middle_finger_mcp.x + hand.ring_finger_mcp.x + hand.pinky_finger_mcp.x + hand.wrist.x) / 5;
-let palmCenterY = (hand.index_finger_mcp.y + hand.middle_finger_mcp.y + hand.ring_finger_mcp.y + hand.pinky_finger_mcp.y + hand.wrist.y) / 5;
-let flameX = palmCenterX;
-let flameY = palmCenterY;
-
-  //Start drawing on the hands here
-  
-flame(flameX, flameY, random(TWO_PI))
-
-    /*
-    Stop drawing on the hands here
-    */
+    if (gesture === "Open Palm") {
+      let palmCenterX = (hand.index_finger_mcp.x + hand.middle_finger_mcp.x + hand.ring_finger_mcp.x + hand.pinky_finger_mcp.x + hand.wrist.x) / 5;
+      let palmCenterY = (hand.index_finger_mcp.y + hand.middle_finger_mcp.y + hand.ring_finger_mcp.y + hand.pinky_finger_mcp.y + hand.wrist.y) / 5;
+      let scaleFactor = map(dist(hand.pinky_finger_tip.x, hand.pinky_finger_tip.y, hand.thumb_tip.x, hand.thumb_tip.y), 30, 200, 0.7, 2.2, true);
+      flame(palmCenterX, palmCenterY, random(TWO_PI), scaleFactor, false, true);
+      continue;
+    }
+    flame(hand.index_finger_tip.x, hand.index_finger_tip.y, random(TWO_PI));
   }
   // You can make addtional elements here, but keep the hand drawing inside the for loop. 
 
   //------------------------------------------------------
 }
 
-function flame (x, y, angle){
-  push ();
-  translate (x, y);
-  rotate (angle);
-
+function flame(x, y, angle, scaleFactor = 1, blueMode = false, redMode = false) {
+  push();
+  translate(x, y);
+  rotate(angle);
   let flicker = random(0.9, 1.1);
-  scale (flicker);
-  noStroke ();
-
-  //outer glow part
-  fill (255, 80, 0, 100)//(255, 80, 0);//orangey red
-  ellipse (0, 0, 80, 100);
-  //main bit
-  fill (255, 140, 0, 180);//(255, 140, 0);//warm orange?
-  beginShape ();
-  vertex (0, 40);
-  bezierVertex (-20, 10, -10, -40, 0, -60);
-  bezierVertex (10, -40, 20, 10, 0, 40);
-  endShape (CLOSE);
-  //inner bit
-  fill (255, 255, 0, 220)//(255, 255, 0);//yellow
-  beginShape();
-  vertex (0, 30);
-  bezierVertex (-10, 5, -5, -30, 0, -40);
-  bezierVertex (5, -30, 10, 5, 0, 30);
-  endShape (CLOSE);
+  scale(flicker * scaleFactor);
+  noStroke();
+  if (redMode) {
+    // vibrant red flame
+    fill(255, 0, 0, 120);
+    ellipse(0, 0, 80, 100);
+    fill(255, 40, 40, 200);
+    beginShape();
+    vertex(0, 40);
+    bezierVertex(-20, 10, -10, -40, 0, -60);
+    bezierVertex(10, -40, 20, 10, 0, 40);
+    endShape(CLOSE);
+    fill(255, 100, 100, 255);
+    beginShape();
+    vertex(0, 30);
+    bezierVertex(-10, 5, -5, -30, 0, -40);
+    bezierVertex(5, -30, 10, 5, 0, 30);
+    endShape(CLOSE);
+  } else if (blueMode) {
+    // blue flame
+    fill(0, 120, 255, 100);
+    ellipse(0, 0, 80, 100);
+    fill(0, 180, 255, 180);
+    beginShape();
+    vertex(0, 40);
+    bezierVertex(-20, 10, -10, -40, 0, -60);
+    bezierVertex(10, -40, 20, 10, 0, 40);
+    endShape(CLOSE);
+    fill(0, 255, 255, 220);
+    beginShape();
+    vertex(0, 30);
+    bezierVertex(-10, 5, -5, -30, 0, -40);
+    bezierVertex(5, -30, 10, 5, 0, 30);
+    endShape(CLOSE);
+  } else {
+    // orange/yellow flame
+    fill(255, 80, 0, 100);
+    ellipse(0, 0, 80, 100);
+    fill(255, 140, 0, 180);
+    beginShape();
+    vertex(0, 40);
+    bezierVertex(-20, 10, -10, -40, 0, -60);
+    bezierVertex(10, -40, 20, 10, 0, 40);
+    endShape(CLOSE);
+    fill(255, 255, 0, 220);
+    beginShape();
+    vertex(0, 30);
+    bezierVertex(-10, 5, -5, -30, 0, -40);
+    bezierVertex(5, -30, 10, 5, 0, 30);
+    endShape(CLOSE);
+  }
   pop();
-  //
-
 }
